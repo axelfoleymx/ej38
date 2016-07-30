9a0d3eac98e16fecaba069659873066e392b1416
¨axelfoleymx¨ <a01164173@gmail.com>
2016-07-29 21:53:40 -0500

    vamos haciendo el array students

--- ej38/app/controllers/application_controller.rb
+++ ej38/app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :set_layout
+
+def set_layout
+if controller_name == "pags" and action_name == "pag2"
+  return 'pag1'
+else
+      return 'application'
+    end
+  end
+
 end
--- ej38/app/controllers/pags_controller.rb
+++ ej38/app/controllers/pags_controller.rb
@@ -4,4 +4,13 @@ class PagsController < ApplicationController
 
   def pag2
   end
+
+  def pag3
+  end
+
+  def get_data
+    @students = []
+    for x in 1..20 do
+      @students << "alumno #{x}"
+    end
 end
